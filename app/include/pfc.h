858e025a1ee500301c963cc9f3f90677f431a235
shulkinj <shulkinj@umich.edu>
2025-02-27 14:03:45 -0500

    Added basic project structure

--- app/include/pfc.h
+++ app/include/pfc.h
@@ -1,3 +1,9 @@
+#include <nrfx_power.h>
+#include <zephyr/kernel.h>
+#include <stdio.h>
+#include <zephyr/drivers/gpio.h>
+
+
 void pof_deep_sleep(void);
 void configure_pof_interrupt(void);
 void power_fail_handler(void);
